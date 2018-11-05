b4eec562a53c13c8fbd2f3e93a7ea477f696de30
Gavin <lugavin@outlook.com>
2018-11-05 12:36:50 +0800

    Create new branch

--- src/components/error/index.js
+++
@@ -1,5 +0,0 @@
-import ForbiddenError from './ForbiddenError';
-import NotFoundError from './NotFoundError';
-import InternalServerError from './InternalServerError';
-
-export { ForbiddenError, NotFoundError, InternalServerError };
